742525e4dab6758b0cc896bd18323c90b4693760
Ana Lopez Perez <ana@marshland.es>
2019-03-24 00:15:50 +0000

    Add background image to cover hero

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './style.scss';
+import "./style.scss";
 
 const Header = () => (
-	<section className="hero is-medium is-primary is-bold">
-		<div className="hero-body">
-			<p className="title has-text-centered is-1 is-spaced">Pylondinium Girls</p>
-			<p className="subtitle has-text-centered is-5">Django workshop</p>
-		</div>
-	</section>
+  <section className="hero is-medium is-primary is-bold cover">
+    <div className="hero-body has-text-centered">
+      <div className="title is-1 is-spaced">Pylondinium Girls</div>
+      <br />
+      <div className="subtitle is-3">
+        Free introductory Django workshop for women
+      </div>
+    </div>
+  </section>
 );
 
 export default Header;
