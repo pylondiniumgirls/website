ccb8cc09d1b4f7833d94d7f9dd13da7a56e7bded
Victor Munoz <victorm@marshland.es>
2019-03-30 22:04:07 +0000

    Updates

---
+++ component---src-pages-404-js-b06229bc02ba87094440.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{142:function(e,t,n){"use strict";n.r(t);var a=n(0),s=n.n(a);t.default=function(){return s.a.createElement("section",{className:"section"},s.a.createElement("h1",null,"NOT FOUND"),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b06229bc02ba87094440.js.map
