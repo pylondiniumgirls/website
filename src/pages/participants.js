4c10af03be078d09754d7113ae950e99d6f7722a
Ana Lopez Perez <ana@marshland.es>
2019-04-14 17:42:17 +0100

    Add clarification to error

--- src/pages/participants.js
+++ src/pages/participants.js
@@ -410,7 +410,7 @@ class MyForm extends React.Component {
               {errors.workingStatus && (
                 <p className="help is-danger">
                   Please select the checkbox that best applies to your current
-                  working status.
+                  working status (only one).
                 </p>
               )}
               {errors.companyName && (
@@ -569,7 +569,7 @@ export default () => (
     <section className="section">
       <h1 className="title has-text-centered">Participant registration</h1>
     </section>
-    <section className="hero is-primary is-bold is-medium">
+    <section className="hero is-primary is-bold is-medium is-hidden-touch is-hidden-tablet">
       <div className="hero-body">
         <div className="container">
           <h1 className="title has-text-centered">
@@ -592,7 +592,7 @@ export default () => (
         </div>
       </div>
     </section>
-    <section className="hero is-primary is-bold is-medium is-hidden-touch is-hidden-tablet">
+    <section className="hero is-primary is-bold is-medium">
       <div className="hero-body">
         <div className="container">
           <h1 className="title has-text-centered">The registration is open!</h1>
@@ -602,7 +602,7 @@ export default () => (
         </div>
       </div>
     </section>
-    <section className="section is-hidden-touch is-hidden-tablet">
+    <section className="section">
       <div className="columns is-centered">
         <div className="column is-half">
           <MyForm />
