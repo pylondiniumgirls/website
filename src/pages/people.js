da2a63762a65ab2625ab5625f4dba49255289791
Ana Lopez Perez <ana@marshland.es>
2019-05-26 19:18:41 +0100

    Add mentors that confirmed until 26May

--- src/pages/people.js
+++ src/pages/people.js
@@ -6,49 +6,111 @@ import Navbar from "../components/navbar";
 import picture_ana from "../images/ana-picture.jpg";
 import bloomberg_logo from "../images/bloomberg-logo.png";
 
-export default () => (
-  <div>
-    <Helmet />
-    <Navbar />
-    <section className="section">
-      <h1 className="title has-text-centered">People</h1>
-      <div className="columns is-centered">
-        <div className="column is-half">
-          <div className="column">
-            This workshop is completely organized by volunteers who have
-            dedicated their time to make this the best possible experience. For
-            that reason, we would like to thank the following people:
+export default () => {
+  const mentors = [
+    {
+      name: "Alex Wilmer",
+      twitter: "",
+      picture:
+        "https://pbs.twimg.com/profile_images/707836806/mugshot-2010_square_small_400x400.jpg",
+      url: "https://github.com/moreati"
+    },
+    {
+      name: "John Sandall",
+      twitter: "@john_sandall",
+      picture:
+        "http://www.artofinference.com/wp-content/uploads/2015/04/john-circle.png",
+      url: "https://coefficient.ai"
+    },
+    {
+      name: "Victor Munoz",
+      twitter: "",
+      picture:
+        "https://en.gravatar.com/userimage/23575299/ac9cc68990ceae99e79c6adfd2e66838.jpg?size=native",
+      url: ""
+    }
+  ];
+
+  const items = [];
+
+  for (const mentor of mentors) {
+    let name;
+    if (mentor.url) {
+      name = (
+        <a href={mentor.url} target="_blank" rel="noopener noreferrer">
+          {mentor.name}
+        </a>
+      );
+    } else {
+      name = <span>{mentor.name}</span>;
+    }
+    items.push(
+      <div className="column is-one-third">
+        <img
+          className="centered people"
+          src={mentor.picture}
+          alt={mentor.name}
+        />
+        <div className="has-text-centered">{name}</div>
+        {mentor.twitter && (
+          <div className="has-text-centered">{mentor.twitter}</div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Helmet />
+      <Navbar />
+      <section className="container">
+        <h1 className="title has-text-centered">People</h1>
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="column">
+              This workshop is completely organized by volunteers who have
+              dedicated their time to make this the best possible experience.
+              For that reason, we would like to thank the following people:
+            </div>
+            <img className="centered people" src={picture_ana} alt="ana" />
+            <div className="content has-text-centered">Ana Lopez</div>
           </div>
-          <img className="centered people" src={picture_ana} alt="ana" />
-          <div className="content has-text-centered">Ana Lopez</div>
         </div>
-      </div>
-    </section>
-    <section className="container">
-      <h3 className="title is-3 has-text-centered">Sponsors</h3>
-      <img className="centered logo" src={bloomberg_logo} alt="bloomberg-logo" />
-    </section>
-    <section className="section">
-      <h3 className="title is-3 has-text-centered">Contact us</h3>
-      <div className="columns is-centered">
-        <div className="column is-half">
-          If you have any questions about the workshop, you have any problems
-          registering as a participant or a mentor, or you want to talk to us
-          about anything in particular, you can contact in our{" "}
-          <a className="emphasis" href="mailto: pylondiniumgirls@gmail.com">
-            email
-          </a>{" "}
-          or you can leave us a message in our{" "}
-          <a
-            className="emphasis"
-            href="https://twitter.com/pylondiniumgir1/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter account
-          </a>
+      </section>
+      <section className="section">
+        <h3 className="title is-3 has-text-centered">Mentors</h3>
+        <div className="columns is-centered">{items}</div>
+      </section>
+      <section className="container">
+        <h3 className="title is-3 has-text-centered">Sponsors</h3>
+        <img
+          className="centered logo"
+          src={bloomberg_logo}
+          alt="bloomberg-logo"
+        />
+      </section>
+      <section className="section">
+        <h3 className="title is-3 has-text-centered">Contact us</h3>
+        <div className="columns is-centered">
+          <div className="column is-half">
+            If you have any questions about the workshop, you have any problems
+            registering as a participant or a mentor, or you want to talk to us
+            about anything in particular, you can contact in our{" "}
+            <a className="emphasis" href="mailto: pylondiniumgirls@gmail.com">
+              email
+            </a>{" "}
+            or you can leave us a message in our{" "}
+            <a
+              className="emphasis"
+              href="https://twitter.com/pylondiniumgir1/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter account
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
-  </div>
-);
+      </section>
+    </div>
+  );
+};
